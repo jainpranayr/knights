5c7e8acaf678692002c9596d9e76578d24c5fd8b
Pranay Jain <jpranay688@gmail.com>
2023-02-26 19:04:52 +0530

    ✨ add KnightIcon in selected cell

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import Cell from "./Cell";
 
 function App() {
+  const [selectedCell, setSelectedCell] = useState<{
+    row: number;
+    col: number;
+  } | null>(null);
+
+  const handleSelectCell = (row: number, col: number) => {
+    if (selectedCell?.row === row && selectedCell?.col === col) {
+      setSelectedCell(null);
+    } else {
+      setSelectedCell({ row, col });
+    }
+  };
+
   return (
     <div className="container mx-auto grid h-screen place-content-center bg-gray-50">
       <div className="grid-rows-8 grid h-full w-full grid-cols-8 border-2 border-gray-500 shadow-md">
@@ -10,8 +24,17 @@ function App() {
           const isEven = (rowIndex + colIndex) % 2 === 0;
 
           return (
-            <div key={`${rowIndex}-${colIndex}`}>
-              <Cell isEven={isEven} />
+            <div
+              key={`${rowIndex}-${colIndex}`}
+              onClick={() => handleSelectCell(rowIndex, colIndex)}
+            >
+              <Cell
+                isEven={isEven}
+                isSelected={
+                  selectedCell?.row === rowIndex &&
+                  selectedCell?.col === colIndex
+                }
+              />
             </div>
           );
         })}
--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,13 +1,29 @@
+import { KnightIcon } from "./icons";
 import { classNames } from "./utils";
 
-function Cell({ isEven }: { isEven: boolean }) {
+function Cell({
+  isEven,
+  isSelected,
+}: {
+  isEven: boolean;
+  isSelected: boolean;
+}) {
   return (
     <div
       className={classNames(
         isEven ? "bg-white" : "bg-gray-500",
-        "items-center` flex h-10 w-10  justify-center md:h-12 md:w-12"
+        "flex h-10 w-10 items-center justify-center md:h-12 md:w-12"
       )}
-    />
+    >
+      {isSelected ? (
+        <KnightIcon
+          className={classNames(
+            isEven ? "text-black" : "text-white",
+            "h-7 w-7"
+          )}
+        />
+      ) : null}
+    </div>
   );
 }
 
