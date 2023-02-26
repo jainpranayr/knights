578d327bc8f45c77a378c9b345a75ce98d47617b
Pranay Jain <jpranay688@gmail.com>
2023-02-26 17:41:48 +0530

    ✨ add chessboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { classNames } from './utils'
+
 function App() {
 	return (
-		<div className='grid place-content-center h-screen text-xl'>
-			Hello, World!
+		<div className='container mx-auto grid place-content-center h-screen bg-gray-50'>
+			<div className='grid grid-cols-8 grid-rows-8 h-full w-full shadow-md border-2 border-gray-500'>
+				{Array.from({ length: 64 }, (_, index) => {
+					const rowIndex = Math.floor(index / 8)
+					const colIndex = index % 8
+					const isEven = (rowIndex + colIndex) % 2 === 0
+
+					return (
+						<div
+							key={`${rowIndex}-${colIndex}`}
+							className={classNames(
+								isEven ? 'bg-white' : 'bg-gray-500',
+								'w-10 h-10 md:w-12 md:h-12  flex justify-center items-center`'
+							)}
+						/>
+					)
+				})}
+			</div>
 		</div>
 	)
 }
